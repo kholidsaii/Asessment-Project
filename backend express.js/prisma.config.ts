b9aeafd50e401ace37bea3fc7593fe44bd15b6ed
Kholid Saifullah <kholidsaifullah@gmail.com>
2026-04-15 19:40:28 +0700

    Cleanup: menghapus folder backend yang tidak terpakai

--- backend express.js/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  datasource: {
-    // Kita tembak langsung filenya ke database Laravel kamu
-    url: "file:C:/project-asessment/backend-api/database/database.sqlite",
-  },
-});
