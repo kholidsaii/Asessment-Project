d8c68fe62f7aa9f9c76b68235e7a3bc2b4a84ad2
Azkia Amanda <azkiaamanda03@gmail.com>
2026-07-02 10:50:18 +0700

    new

--- BE_express-js/index.js
+++ BE_express-js/index.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 
 const express = require("express");
+const cors = require("cors");
 const router = require("./routes/api");
 const path = require("path"); // [1] Tambahkan modul path untuk handle direktori
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true
+}));
 
 // Middleware
 app.use(express.json());
