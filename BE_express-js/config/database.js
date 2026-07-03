1a5361fad3e5099041085b081c512360ce609952
Azkia Amanda <azkiaamanda03@gmail.com>
2026-07-03 08:17:33 +0700

    sprint 13 - fiksasi kode

--- BE_express-js/config/database.js
+++ BE_express-js/config/database.js
@@ -2,11 +2,9 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : undefined,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
